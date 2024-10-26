5cfeb97bb5835b1075d9e936b3c66d8ee5a4c61e
dylankohn <dylankohn1@gmail.com>
2024-10-26 13:08:18 -0600

    fixed a bug

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -3,7 +3,7 @@ using System;
 class Scripture
 {
     //displays scripture
-    public string _scripture = "The Spirit itself beareth witness with our spirit that we are the children of God";
+    private string _scripture = "The Spirit itself beareth witness with our spirit that we are the children of God";
     public string DisplayScripture()
     {
         return _scripture;
