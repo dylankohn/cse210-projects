e566b1b00d234c0101f365835afce32c55965ea2
dylankohn <dylankohn1@gmail.com>
2024-10-31 18:16:18 -0600

    W07 Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("John Doe", "Math"); 
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Jackson Johnson", "History", "Chapter 1", "1-10");
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Alice Greedy", "Writing", "My Essay"); 
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
